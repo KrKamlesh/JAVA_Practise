c62168309a8f63de9d3f53d1b2e05efa25a29f2f
Kamlesh <Kamlesh@DESKTOP-SKR045R>
2021-03-15 08:58:03 +0530

    Update

--- src/Programmepractise/Palidrome.java
+++ src/Programmepractise/Palidrome.java
@@ -1,6 +1,28 @@
 package Programmepractise;
 
-public class Palidrome 
-{
- 
+public class Palidrome {
+
+	public static void main(String[] args) {
+		
+	int num=121;
+	int rem=0
+			;
+	int rev=0;
+	int temp=num;
+	while(num>0)
+	{
+		rem=num%10;
+		rev=(rev*10)+rem;
+		num=num/10;
+	}
+	if(temp==num)
+	{
+		System.out.println("The number is palidrome");
+	}
+	else
+	{
+		System.out.println("The number is not palidrome");
+	}
+	}
+
 }
